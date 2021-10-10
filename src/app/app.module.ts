ea55c66f75de3d4fc3b266d0ae2c814d79013389
vikrant754 <56115126+vikrant754@users.noreply.github.com>
2021-10-10 16:02:33 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
